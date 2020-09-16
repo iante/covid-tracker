a427089d49be6e1cb869ee6d75d8a462935919ef
iante <31571006+iante@users.noreply.github.com>
2020-09-16 16:50:55 +0300

    Update InfoBox.js

--- covid/src/InfoBox.js
+++ covid/src/InfoBox.js
@@ -2,17 +2,11 @@ import React from 'react';
 import  './InfoBox.css';
 import {Card, CardContent, Typography} from '@material-ui/core';
 
-//what we are passing i.e title, cases and total are called props
+
 function InfoBox({title, cases, active, total, isRed, ...props}){
 
     return(
-     /*since we are using InfoBox which is material-ui, we cannot define
-     methods like onClic directly. By spreading it as a props i.e ...props, we are able
-     to use the onclick and call it as below by onClick={props.onClick} */
-
-     /* The ${active && "infoBox--selected"} ${
-            isRed && "infoBox--red" checks if is active and applies the infoBox--selected class to it
-            it also checks if isred and applies infoBox--red class */
+     
         <Card onClick={props.onClick} className={`infoBox ${active && "infoBox--selected"} ${
             isRed && "infoBox--red"
           }`}> 
